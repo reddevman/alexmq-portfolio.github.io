3896263596f731c3d1c2532f6b5cfefc44e641bd
reddevman <alemarqred@gmail.com>
2021-02-23 20:49:21 +0100

    key press

--- ejercicios/Entorno-Cliente/TRIMESTRE2/EventoKey/script.js
+++ ejercicios/Entorno-Cliente/TRIMESTRE2/EventoKey/script.js
@@ -1,9 +1,34 @@
-window.onkeydown = function (event) {
-    if (event.keyCode === 112) {
-        document.getElementById("texto").innerHTML = "F1 pulsada";
-    } else if (event.keyCode !== 112) {
-        document.getElementById("texto").style.display = "Pulsa F1 para ayuda";
-    } else {
-        document.getElementById("texto").style.display = "none";
+var keyTwice = false;
+
+window.addEventListener("keydown", function (event) {
+
+    event.preventDefault();
+
+    var key = event.key || event.keyCode;
+    if (key === "F1") {
+        document.querySelector("#texto").innerHTML = "F1 PULSADA. Pulsa F1 para eliminar el mensaje.";
+        if (keyTwice) {
+            document.querySelector("#texto").innerHTML = "";
+        }
+        keyTwice = true;
+        setTimeout(function () {
+            keyTwice = 0;
+        }, 1000);
     }
-};
+
+    if (key !== "F1") {
+        document.querySelector("#texto").innerHTML = "Pulsa F1 para ayuda.";
+    }
+});
+
+// window.addEventListener("keyup", function (event)
+// {
+//     if (event.defaultPrevented) {
+//         return;
+//     }
+
+//     var key = event.key || event.keyCode;
+//     if (key === "F1") {
+//         document.querySelector("#texto").innerHTML = "";
+//     }
+// });
